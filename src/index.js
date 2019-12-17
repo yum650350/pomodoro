3886e3176b32c4d46db6fe72462064ebae444c22
TC-MIKE <yum650350@gmail.com>
2019-12-17 22:51:30 +0800

    serviceWorker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Pomodoro from "./Pomodoro";
+import * as serviceWorker from './serviceWorker';
 
 if (document.getElementById("root")) {
   ReactDOM.render(<Pomodoro />, document.getElementById("root"));
@@ -10,4 +11,4 @@ if (document.getElementById("root")) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.register();
